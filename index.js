02363b7d34bfda8ba5b893647fd545162358f9ee
axel <laxels@gmail.com>
2020-12-30 11:58:26 -0700

    invoke next() after sending response

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ module.exports = function proxy(host, userOptions) {
       .then(decorateUserResHeaders)
       .then(decorateUserRes)
       .then(sendUserRes)
+      .then(() => {
+        if (next != null) {
+          next();
+        }
+      })
       .catch(function (err) {
         // I sometimes reject without an error to shortcircuit the remaining
         // steps and return control to the host application.
